de6c031776c7e250c97340687f53dd89fa878fac
Syed Ali <syedali3267@gmail.com>
2021-05-12 16:29:28 +0100

    Added an about page and added a view and made sure URL's knew whee to find it

--- propertyapp/views.py
+++ propertyapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render (request, 'search.html')
+    return render(request, 'search.html')
+
+
+def about(request):
+    return render(request, 'about.html')
