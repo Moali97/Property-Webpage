cd0ccbd9c1feb970ad8163b9b9d329365b7d7d99
Syed Ali <syedali3267@gmail.com>
2021-05-14 20:10:36 +0100

    Configured search bar to go to listings page

--- propertylistings/views.py
+++ propertylistings/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def search_listings(request):
-    return render(request, 'propertylistings.html', {})
+    if request.method == "POST":
+        searched = request.POST['searched']
+
+        return render(request, 'propertylistings.html', {'searched':searched})
+    else:
+        return render(request, 'propertylistings.html', {})
