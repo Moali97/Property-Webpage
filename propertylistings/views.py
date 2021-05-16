1a9f94618be99b902bc2039b389c9c09f5efe28e
Syed Ali <syedali3267@gmail.com>
2021-05-16 12:18:36 +0100

    Removed extra brackets from context, this allowed searched objects to be shown

--- propertylistings/views.py
+++ propertylistings/views.py
@@ -1,10 +1,16 @@
+import json
 from django.shortcuts import render
+from .models import Propertydetails
 
 
 def search_listings(request):
     if request.method == "POST":
         searched = request.POST['searched']
+        listings = Propertydetails.objects.filter(city__contains=searched)
 
-        return render(request, 'propertylistings.html', {'searched':searched})
-    else:
-        return render(request, 'propertylistings.html', {})
+        return render(request, 'propertylistings.html',
+                      {'searched':searched,
+                      'listings':listings})
+    #else:
+
+        #return render(request, 'propertylistings.html', {})
