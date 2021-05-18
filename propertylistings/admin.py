206b60041db5ba7740e182a8f7f3ae7df9fcc2b9
Syed Ali <syedali3267@gmail.com>
2021-05-18 17:42:01 +0100

    Added 'list_display' in admin to show full list f page

--- propertylistings/admin.py
+++ propertylistings/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from propertylistings.models import Propertydetails
 
-admin.site.register(Propertydetails)
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("city", "description", "address", "bedrooms", "bathrooms")
+
+
+admin.site.register(Propertydetails, PropertyAdmin)
