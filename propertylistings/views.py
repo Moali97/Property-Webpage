4b0a2cdd3498a964525aaf9061bd88a057bbae42
Syed Ali <syedali3267@gmail.com>
2021-05-18 16:00:42 +0100

    Added spaces for readability

--- propertylistings/views.py
+++ propertylistings/views.py
@@ -5,12 +5,15 @@ from .models import Propertydetails
 
 def search_listings(request):
     if request.method == "POST":
+
         searched = request.POST['searched']
+
         listings = Propertydetails.objects.filter(city__contains=searched)
 
         return render(request, 'propertylistings.html',
-                      {'searched':searched,
-                      'listings':listings})
-    #else:
+                      {'searched': searched,
+                       'listings': listings})
+    else:
 
-        #return render(request, 'propertylistings.html', {})
+        return render(request, 'propertylistings.html', {})
+
