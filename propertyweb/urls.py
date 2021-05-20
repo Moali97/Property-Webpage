b88f9be8b390cd272afbdc3116d8f766fb85ef42
Syed Ali <syedali3267@gmail.com>
2021-05-20 17:04:36 +0100

    Added MEDIA root and code to allow images of property to appear when searched

--- propertyweb/urls.py
+++ propertyweb/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from propertyapp import views
-from propertylistings import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('propertyapp.urls')),
     path('', include('propertylistings.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
