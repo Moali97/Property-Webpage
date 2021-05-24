43401a34a1068fb1be9ebe07968d272e2bf4fc6e
Syed Ali <syedali3267@gmail.com>
2021-05-24 11:10:37 +0100

    Temporaily removed whitenoise from installed

--- propertyweb/settings.py
+++ propertyweb/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'whitenoise.runserver_nostatic',
+    #'whitenoise.runserver_nostatic',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
