7f23afee98c37a78f2cb8dc0c90b2db7cbf31aa3
Syed Ali <syedali3267@gmail.com>
2021-06-09 12:18:44 +0100

    temp change

--- propertyweb/settings.py
+++ propertyweb/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # put images2
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images2') # put images2
 
 # HTTPS settings
 SESSION_COOKIE_SECURE = True
