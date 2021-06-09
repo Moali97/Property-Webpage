3f622286fd411f8d179230cce62c872cd7a706c3
Syed Ali <syedali3267@gmail.com>
2021-06-09 12:40:52 +0100

    temp change

--- propertyweb/settings.py
+++ propertyweb/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images2') # put images2
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # put images2
 
 # HTTPS settings
 SESSION_COOKIE_SECURE = True
